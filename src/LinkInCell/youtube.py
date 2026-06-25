df7e29d38896facb3db5ffa888a7266c7a39c884
Readwanur <melodygangster@gmail.com>
2026-06-26 00:29:45 +0600

    youtube render added

---
+++ src/LinkInCell/youtube.py
@@ -0,0 +1,32 @@
+from IPython.display import HTML, display
+from LinkInCell.custom_exception import InvalidURLException
+from LinkInCell.logger import logger
+import re
+
+def render_youtube(url: str, width: int = 780, height: int = 440):
+  try:
+    regex = r"(?:v=|\/)([0-9A-Za-z_-]{11}).*"
+    match = re.search(regex, url)
+
+    if not match:
+      raise InvalidURLException("Invalid YouTube URL")
+
+    video_id = match.group(1)
+    embed_url = f"https://www.youtube-nocookie.com/embed/{video_id}"
+
+    iframe = f"""
+        <iframe width="{width}" height="{height}" 
+        src="{embed_url}" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen>
+        </iframe>
+        """ 
+    display(HTML(iframe))
+    logger.info(f"Successfully rendered YouTube video for URL: {url}.")
+
+    return "Success."
+  except Exception as e:
+      raise e
